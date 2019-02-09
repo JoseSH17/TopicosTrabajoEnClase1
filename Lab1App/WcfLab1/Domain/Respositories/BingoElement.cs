b0cc317b32b1e9c7fe348ccbc41280e25783729e
kevinML77 <40698565+kevinML77@users.noreply.github.com>
2019-02-08 23:14:09 -0600

    Update BingoElement.cs

--- Lab1App/WcfLab1/Domain/Respositories/BingoElement.cs
+++ Lab1App/WcfLab1/Domain/Respositories/BingoElement.cs
@@ -7,9 +7,11 @@ namespace WcfLab1.Domain.Respositories
 {
     public class BingoElement
     {
+        // Declaracion de variables
         public string Number { get; set; }
         public bool State { get; set; }
 
+        // Constructor
         public BingoElement(string _Number, bool _State)
         {
             this.Number = _Number;
